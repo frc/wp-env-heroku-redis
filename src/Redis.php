32b660494d71c0e394ae97337c73e36794f1fae7
Josep Roca <josep.roca@frantic.com>
2017-02-08 17:41:30 +0200

    support for saving PHP sessions from redis

--- src/Redis.php
+++ src/Redis.php
@@ -8,4 +8,9 @@ if ( $env ) {
     define('WP_REDIS_HOST', trim( $url['host'] ));
     define('WP_REDIS_PORT', trim( $url['port'] ));
     define('WP_REDIS_PASSWORD', trim( $url['pass'] ));
+
+    if (getenv('REDIS_SESSION_STORAGE')) {
+        ini_set('session.save_handler','redis');
+        ini_set('session.save_path',"tcp://$url[host]:$url[port]?auth=$url[pass]");
+    }
 }
