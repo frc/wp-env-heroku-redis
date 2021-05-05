db809e86eee3423f815f24151e8dd12ab9143e05
Lauri Kallioniemi <lauri.kallioniemi@frantic.com>
2021-05-05 19:02:52 +0300

    enable heroku redis 6

--- src/Redis.php
+++ src/Redis.php
@@ -2,15 +2,28 @@
 
 namespace Frc\WP\Env\Heroku\Redis;
 
-$env = getenv('REDIS_URL') ? getenv('REDIS_URL') : getenv('REDISCLOUD_URL');
+if (getenv('REDIS_TLS_URL')){
+    $env = getenv('REDIS_TLS_URL');
+} else if (getenv('REDIS_URL')){
+    $env = getenv('REDIS_URL');
+} else {
+    $env = getenv('REDISCLOUD_URL');
+}
 if ( $env ) {
     $url = parse_url($env);
+    $scheme = 'tcp';
     define('WP_REDIS_HOST', trim( $url['host'] ));
     define('WP_REDIS_PORT', trim( $url['port'] ));
     define('WP_REDIS_PASSWORD', trim( $url['pass'] ));
 
+    if ($url['scheme'] == 'rediss') {
+        $scheme = 'tlsv1.2';
+        define('WP_REDIS_SCHEME', $scheme);
+        define('WP_REDIS_STREAM_CONTEXT', ['stream' => ['verify_peer' => false, 'verify_peer_name'  => false, 'allow_self_signed' => true ]]);
+    }
+
     if (getenv('REDIS_SESSION_STORAGE') && getenv('REDIS_SESSION_STORAGE') === 'true') {
         ini_set('session.save_handler','redis');
-        ini_set('session.save_path',"tcp://$url[host]:$url[port]?auth=$url[pass]");
+        ini_set('session.save_path',"$scheme://$url[host]:$url[port]?auth[user]=$url[user]&auth[pass]=$url[pass]");
     }
 }
